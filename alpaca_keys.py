7e6a8ded58caeaafda797c211b2fb52a6881c01c
Dhiman Roy <dhimanroy@Dhimans-MacBook-Air.local>
2026-05-11 22:12:36 -0500

    remove keys

--- alpaca_keys.py
+++
@@ -1,9 +0,0 @@
-secret_key = "83byWpkXvbV4DTxcUa7ra9BPp6v7sPTRiVyEPbn2rW7g"
-api_key = "PKTMIP3BQAIQ4DCTP2XCRCI4OX"
-
-
-class AlpacaKeys(object):
-    def __init__(self):
-        self.secret_key = secret_key
-        self.api_key = api_key
-
